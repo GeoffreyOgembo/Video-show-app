1643aef15753af3801533e2ec82704c62b9bf962
geoffreyogembo <geoffreyogembo06@gmail.com>
2022-07-28 12:18:08 -0400

    resolved errors in react dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 
   
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 
 // State
 import ShowsState from "./context/shows/ShowsState";
 import AlertsState from "./context/alerts/AlertsState";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ShowsState>
+   <AlertsState>
+    <App />
+    </AlertsState>
+   </ShowsState>,
+  </React.StrictMode>
+);
+
+reportWebVitals();
 
 // const root = ReactDOM.createRoot(document.getElementById('root'),
 // root.render(
@@ -20,13 +34,13 @@ import AlertsState from "./context/alerts/AlertsState";
 // )
 // )
 
-ReactDOM.render(
-  <ShowsState>
-    <AlertsState>
-      <App />
-    </AlertsState>
-  </ShowsState>,
-  document.getElementById("root")
-);
+// ReactDOM.render(
+//   <ShowsState>
+//     <AlertsState>
+//       <App />
+//     </AlertsState>
+//   </ShowsState>,
+//   document.getElementById("root")
+// );
 
-reportWebVitals();
+// reportWebVitals();
